7391bee86ee6816bac62a05c651bc4ec2190977c
Lawson Chibueze <speak2lawson@gmail.com>
2023-06-21 20:04:16 +0100

    First Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import { Amplify } from "aws-amplify";
+import config from "../../aws-exports";
+Amplify.configure({ ...config, ssr: true });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
